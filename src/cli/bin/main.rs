00cb0a4c40dcbe54c27849c1daa24f74f76afd97
fl3xice <two.fl3xice@yandex.ru>
2022-05-05 01:03:19 +0300

    logging

--- src/cli/bin/main.rs
+++ src/cli/bin/main.rs
@@ -1,6 +1,7 @@
-use std::net::SocketAddrV4;
-
 use clap::Parser;
+use env_logger::{Builder, Target};
+use log::LevelFilter;
+use std::net::SocketAddrV4;
 
 #[derive(Parser, Debug)]
 #[clap(author = "flexice", version, about, long_about = None)]
@@ -8,11 +9,28 @@ struct Args {
     #[clap(long)]
     connect_to_another_server: Option<SocketAddrV4>,
 
-    /// Number of times to greet
-    #[clap(short, long, default_value_t = 1)]
-    count: u8,
+    #[clap(long, parse(try_from_str), default_value = "false")]
+    verbose: bool,
 }
 
+/**
+
+### Derive
+[Open](https://github.com/clap-rs/clap/blob/v3.1.15/examples/derive_ref/README.md#arg-attributes)
+
+### Builder
+[Open](https://github.com/clap-rs/clap/blob/v3.1.15/examples/tutorial_builder/README.md)
+
+*/
 fn main() {
-    let _args = Args::parse();
+    let args = Args::parse();
+
+    if args.verbose {
+        Builder::new()
+            .filter_level(LevelFilter::Debug)
+            .target(Target::Stdout)
+            .init();
+    } else {
+        Builder::new().target(Target::Stdout).init();
+    }
 }
